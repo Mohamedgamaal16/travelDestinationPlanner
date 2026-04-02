8fc40aeba4220ca91b266729b4bba08e717b20c1
mohamedgamalcode81 <mohamed.sayed@code81.com>
2026-04-02 13:25:51 +0200

    init backend

---
+++ backend/src/main/java/com/travelDestinationPlanner/fawry/FawryApplication.java
@@ -0,0 +1,13 @@
+package com.travelDestinationPlanner.fawry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FawryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FawryApplication.class, args);
+	}
+
+}
